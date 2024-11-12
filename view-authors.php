c062c6a4201cc08f3a228febb80736ed16c3b169
ClaireYarber <clyarber@ou.edu>
2024-11-11 21:15:44 -0600

    Update view-authors.php

--- view-authors.php
+++ view-authors.php
@@ -3,9 +3,10 @@
   <table class="table">
     <thead>
       <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Publisher</th>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Publisher</th>
+        <th>Books</th>
       </tr>
     </thead>
     <tbody>
@@ -16,6 +17,17 @@ while ($author = $authors->fetch_assoc()) {
     <td><?php echo $author['author_id']; ?></td>
     <td><?php echo $author['author_name']; ?></td>
     <td><?php echo $author['publisher']; ?></td>
+    <td>
+      <ul>
+      <?php
+      while ($book = $author['books']->fetch_assoc()) {
+      ?>
+        <li><?php echo $book['book_name']; ?></li>
+      <?php
+      }
+      ?>
+      </ul>
+    </td>
   </tr>
 <?php
 }
