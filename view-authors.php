24685c1d02dca673443faccc2be7df54465b72cc
ClaireYarber <clyarber@ou.edu>
2024-11-11 19:45:45 -0600

    Update view-authors.php

--- view-authors.php
+++ view-authors.php
@@ -1,1 +1,25 @@
-
+<h1>Authors</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Publisher</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($author = $authors->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $author['author_id']; ?></td>
+    <td><?php echo $author['author_name']; ?></td>
+    <td><?php echo $author['publisher']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
