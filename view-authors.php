aadf20e2181b12b6a94fbae13d393827931ff69e
ClaireYarber <clyarber@ou.edu>
2024-11-11 21:21:23 -0600

    Update view-authors.php

--- view-authors.php
+++ view-authors.php
@@ -3,10 +3,9 @@
   <table class="table">
     <thead>
       <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Publisher</th>
-        <th>Books</th>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Publisher</th>
       </tr>
     </thead>
     <tbody>
@@ -15,23 +14,11 @@ while ($author = $authors->fetch_assoc()) {
 ?>
   <tr>
     <td><?php echo $author['author_id']; ?></td>
-    <td><a href="books.php?author_id=<?php echo $author['author_id']; ?>"><?php echo $author['author_name']; ?></a></td>
+    <td><?php echo $author['author_name']; ?></td>
     <td><?php echo $author['publisher']; ?></td>
-    <td>
-      <ul>
-      <?php
-      while ($book = $author['books']->fetch_assoc()) {
-      ?>
-        <li><?php echo $book['book_name']; ?></li>
-      <?php
-      }
-      ?>
-      </ul>
-    </td>
   </tr>
 <?php
 }
 ?>
     </tbody>
-  </table>
-</div>
+  </tabl
