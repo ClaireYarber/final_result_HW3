fa2b585d437bad7ac50cecbbe7adfd146707ad1b
ClaireYarber <clyarber@ou.edu>
2024-11-11 20:10:56 -0600

    Update view-books.php

--- view-books.php
+++ view-books.php
@@ -1,1 +1,25 @@
-
+<h1>Books</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($book = $books->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $book['book_id']; ?></td>
+    <td><?php echo $book['book_name']; ?></td>
+    <td><?php echo $book['book_description']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
