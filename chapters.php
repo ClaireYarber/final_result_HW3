f21a583e64c7080c638b7a4ff4d865e4768161f0
ClaireYarber <clyarber@ou.edu>
2024-11-11 20:34:20 -0600

    Update chapters.php

--- chapters.php
+++ chapters.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-chapters.php");
 
+$pageTitle= "Chapters";
+include "view-header.php";
+$chapters = selectChapters();
+include "view-chapters.php";
+include "view-footer.php";
+?>
