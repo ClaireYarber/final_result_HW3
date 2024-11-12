4bdc7afe9307425735e3516e18f774187e309ecc
ClaireYarber <clyarber@ou.edu>
2024-11-11 21:17:20 -0600

    Update books.php

--- books.php
+++ books.php
@@ -5,7 +5,7 @@ require_once("model-books.php");
 $pageTitle= "Books";
 include "view-header.php";
 
-$author_id = isset($_GET['author_id']) ? intval($_GET['author_id']) : 0;
+$author_id = isset($_POST['author_id']) ? intval($_POST['author_id']) : 0;
 
 if ($author_id > 0) {
     $books = selectBooksByAuthor($author_id);
