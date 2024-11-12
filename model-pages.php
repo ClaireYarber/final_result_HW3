3bf7f04b2ef397ca24192dc329ba0f5be9089dff
ClaireYarber <clyarber@ou.edu>
2024-11-11 20:16:44 -0600

    Update model-pages.php

--- model-pages.php
+++ model-pages.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectPages() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT page_id, author_id, book_id, year, month, day FROM `page`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
