f66c0637a402d2845e5ea057a0467de088a178c6
ClaireYarber <clyarber@ou.edu>
2024-11-11 20:15:00 -0600

    Update pages.php

--- pages.php
+++ pages.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-pages.php");
 
+$pageTitle= "Pages";
+include "view-header.php";
+$pages = selectPages();
+include "view-pages.php";
+include "view-footer.php";
+?>
