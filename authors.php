08f1e0fdc48a913ce7993257865363d13c56a989
ClaireYarber <clyarber@ou.edu>
2024-11-11 21:18:33 -0600

    Update authors.php

--- authors.php
+++ authors.php
@@ -1,17 +1,10 @@
 <?php
 require_once("util-db.php");
 require_once("model-authors.php");
-require_once("model-books.php");
 
-$pageTitle = "Authors";
+$pageTitle= "Authors";
 include "view-header.php";
-
 $authors = selectAuthors();
-
-foreach ($authors as $author) {
-    $author['books'] = selectBooksByAuthor($author['author_id']);
-}
-
 include "view-authors.php";
 include "view-footer.php";
 ?>
