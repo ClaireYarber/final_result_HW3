1d72eb91b6bb4904239d981eb7f738b5118f5764
ClaireYarber <clyarber@ou.edu>
2024-11-11 18:47:50 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,1 +1,12 @@
-
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('138.197.17.168', 'misclair_newHW3user', 'CY41727jms!', 'misclair_newHW3');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
