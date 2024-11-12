3e72244e9ae7cf76f443bc85e96ab471b78bca91
ClaireYarber <clyarber@ou.edu>
2024-11-11 20:25:25 -0600

    Update authors-with-books.php

--- authors-with-books.php
+++ authors-with-books.php
@@ -4,7 +4,7 @@ require_once("model-authors-with-books.php");
 
 $pageTitle= "Authors With Books";
 include "view-header.php";
-$authors-with-books = selectPages();
+$authors-with-books = selectAuthorWithBooks();
 include "view-authors-with-books.php";
 include "view-footer.php";
 ?>
