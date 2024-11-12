ef877eb1ed194a76fc283da5d82e71901409a6b6
ClaireYarber <clyarber@ou.edu>
2024-11-11 20:09:03 -0600

    Update model-books.php

--- model-books.php
+++ model-books.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectBooks() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT book_id, book_name, book_description FROM `book`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
