55a2cb42cdc24ae4a33e5bf5f8dbf12404baf0a4
ClaireYarber <clyarber@ou.edu>
2024-11-11 18:19:39 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,14 +20,12 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
+          <a class="nav-link" href="authors.php">Authors</a>
         </li>
      <li class="nav-item">
-          <a class="nav-link" href="post-result.php">Post result</a>
+          <a class="nav-link" href="books.php">Books</a>
         </li><li class="nav-item">
-          <a class="nav-link" href="get.php">Get</a>
-        </li><li class="nav-item">
-          <a class="nav-link" href="get-result.php">Get result</a>
+          <a class="nav-link" href="authors-with-books.php">Authors With Books</a>
         </li> 
       </ul>     
     </div>
