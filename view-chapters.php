ed0242462e62cf8313f20ae4e84be3484829a87a
ClaireYarber <clyarber@ou.edu>
2024-11-11 20:41:06 -0600

    Update view-chapters.php

--- view-chapters.php
+++ view-chapters.php
@@ -15,7 +15,7 @@ while ($book = $chapters->fetch_assoc()) {
   <tr>
     <td><?php echo $chapter['chapter_id']; ?></td>
     <td><?php echo $chapter['chapter_name']; ?></td>
-    <td><?php echo $chapter['$chapter_number']; ?></td>
+    <td><?php echo $chapter['chapter_number']; ?></td>
   </tr>
 <?php
 }
