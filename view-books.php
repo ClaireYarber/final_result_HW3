1266d5412d1e4c2582dbcdfb83226c0e20c21380
ClaireYarber <clyarber@ou.edu>
2024-11-11 21:16:59 -0600

    Update view-books.php

--- view-books.php
+++ view-books.php
@@ -1,11 +1,28 @@
 <h1>Books</h1>
+<form method="post" action="books.php">
+  <div class="form-group">
+    <label for="author_id">Filter by Author:</label>
+    <select name="author_id" id="author_id" class="form-control">
+      <?php
+      $authors = selectAuthors();
+      while ($author = $authors->fetch_assoc()) {
+      ?>
+        <option value="<?php echo $author['author_id']; ?>"><?php echo $author['author_name']; ?></option>
+      <?php
+      }
+      ?>
+    </select>
+  </div>
+  <button type="submit" class="btn btn-primary">Filter</button>
+</form>
+
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Description</th>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Description</th>
       </tr>
     </thead>
     <tbody>
