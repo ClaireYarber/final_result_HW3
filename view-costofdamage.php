748980d03aece043a902a0e371dea5c2dcf97068
ClaireYarber <clyarber@ou.edu>
2024-12-12 15:27:42 -0600

    Update view-costofdamage.php

--- view-costofdamage.php
+++ view-costofdamage.php
@@ -1,4 +1,12 @@
-<h1>Cost of damage to Car</h1>
+<h1>Cost of Damage to Car</h1>
+<div class="row">
+  <div class="col">
+    <h2>Cost of Damage</h2>
+  </div>
+  <div class="col-auto">
+    <?php include "view-costofdamage-newform.php"; ?>
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
@@ -6,23 +14,32 @@
         <th>ID</th>
         <th>Brand</th>
         <th>Cost</th>
-        <th>F1 Driver ID</th>   
+        <th>F1 Driver ID</th>
+        <th>Edit</th>
+        <th>Delete</th>
       </tr>
     </thead>
     <tbody>
-      <?php
-
-      while ($costofdamages = $costofdamage->fetch_assoc()) { 
-      ?>
+      <?php while ($costofdamages = $costofdamage->fetch_assoc()) { ?>
         <tr>
           <td><?php echo $costofdamages['costofdamage_id']; ?></td>
           <td><?php echo $costofdamages['cod_brand']; ?></td>
           <td><?php echo $costofdamages['cod_cost']; ?></td>
           <td><?php echo $costofdamages['f1driver_id']; ?></td>
+          <td>
+            <?php include "view-costofdamage-editform.php"; ?>
+          </td>
+          <td>
+            <form method="post" action="">
+              <input type="hidden" name="costofdamage_id" value="<?php echo $costofdamages['costofdamage_id']; ?>">
+              <input type="hidden" name="actionType" value="Delete">
+              <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this record?');">
+                Delete
+              </button>
+            </form>
+          </td>
         </tr>
-      <?php
-      } 
-      ?>
+      <?php } ?>
     </tbody>
   </table>
 </div>
