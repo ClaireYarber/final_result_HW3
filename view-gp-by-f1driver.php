6f2375af23f7653de4b19372f84a6a85c9e72e85
ClaireYarber <clyarber@ou.edu>
2024-12-12 13:42:14 -0600

    Update view-gp-by-f1driver.php

--- view-gp-by-f1driver.php
+++ view-gp-by-f1driver.php
@@ -1,30 +1,3 @@
-// <h1>F1 Grand Prix's</h1>
-// <div class="table-responsive">
-//   <table class="table">
-//     <thead>
-//       <tr>
-//         <th>ID</th>
-//         <th>Number</th>
-//         <th>Points</th>
-//       </tr>
-//     </thead>
-//     <tbody>
-//       <?php
-
-//       while ($ranks = $rank->fetch_assoc()) { 
-//       ?>
-//         <tr>
-//           <td><?php echo $ranks['rank_id']; ?></td>
-//           <td><?php echo $ranks['rank_number']; ?></td>
-//           <td><?php echo $ranks['total_points']; ?></td>
-//         </tr>
-//       <?php
-//       } 
-//       ?>
-//     </tbody>
-//   </table>
-// </div>
-
 <h1>F1 Grand Prix's</h1>
 <div class="table-responsive">
   <table class="table">
