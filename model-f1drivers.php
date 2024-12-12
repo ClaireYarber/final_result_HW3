4cb15f0412061c09909503562321597041ec6046
ClaireYarber <clyarber@ou.edu>
2024-12-12 13:02:56 -0600

    Update model-f1drivers.php

--- model-f1drivers.php
+++ model-f1drivers.php
@@ -1,8 +1,8 @@
 <?php
-function selectf1drivers() {
+function selectF1Drivers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT f1driver_id, f1driver_name, country FROM `f1driver` ");
+        $stmt = $conn->prepare("SELECT f1driver_id, f1driver_name, country FROM `f1driver`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -12,4 +12,47 @@ function selectf1drivers() {
         throw $e;
     }
 }
+
+function insertF1Driver($name, $country) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `f1driver` (`f1driver_name`, `country`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $name, $country);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateF1Driver($name, $country, $id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `f1driver` SET `f1driver_name` = ?, `country` = ? WHERE `f1driver_id` = ?");
+        $stmt->bind_param("ssi", $name, $country, $id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteF1Driver($id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `f1driver` WHERE `f1driver_id` = ?");
+        $stmt->bind_param("i", $id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
+
