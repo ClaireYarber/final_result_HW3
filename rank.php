3c32993a40979c84b238a211a9f05c1252432c81
ClaireYarber <clyarber@ou.edu>
2024-12-12 13:11:03 -0600

    Update rank.php

--- rank.php
+++ rank.php
@@ -4,7 +4,25 @@ require_once("model-rank.php");
 
 $pageTitle = "Rank";
 include "view-header.php";
-$rank = selectrank ();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $actionType = $_POST['actionType'];
+    $rank_number = $_POST['rank_number'];
+    $total_points = $_POST['total_points'];
+
+    if ($actionType == "Add") {
+        insertRank($rank_number, $total_points);
+    } elseif ($actionType == "Edit") {
+        $rank_id = $_POST['rank_id'];
+        updateRank($rank_id, $rank_number, $total_points);
+    } elseif ($actionType == "Delete") {
+        $rank_id = $_POST['rank_id'];
+        deleteRank($rank_id);
+    }
+}
+
+$rank = selectRank();
 include "view-rank.php";
+include "view-rank-newform.php";
 include "view-footer.php";
 ?>
