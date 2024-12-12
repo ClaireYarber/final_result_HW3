53e55c24317deb2789aa82d875ea068824c664bd
ClaireYarber <clyarber@ou.edu>
2024-12-11 23:49:53 -0600

    Update model-f1driver-with-gp.php

--- model-f1driver-with-gp.php
+++ model-f1driver-with-gp.php
@@ -1,5 +1,6 @@
 <?php
 function selectf1drivers() {
+    $conn = null;
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT f1driver_id, f1driver_name, country FROM `f1driver` ");
@@ -8,25 +9,30 @@ function selectf1drivers() {
         $conn->close();
         return $result;
     } catch (Exception $e) {
-        $conn->close();
+        if ($conn) {
+            $conn->close();
+        }
         throw $e;
     }
 }
 ?>
+
 <?php
 function selectgpbyf1driver($fid) {
+    $conn = null;
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT r.rank_id, rank_number, total_points, gp_name, country, day_time FROM `rank` r join gp g on g.rank_id = r.rank_id where g.f1driver_id=?");
+        $stmt = $conn->prepare("SELECT r.rank_id, rank_number, total_points, gp_name, country, day_time FROM `rank` r JOIN gp g ON g.rank_id = r.rank_id WHERE g.f1driver_id = ?");
         $stmt->bind_param("i", $fid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
         return $result;
     } catch (Exception $e) {
-        $conn->close();
+        if ($conn) {
+            $conn->close();
+        }
         throw $e;
     }
 }
 ?>
-
