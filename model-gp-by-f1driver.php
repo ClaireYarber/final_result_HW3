8255d99b1df183ca3b7eb6ff9f5cf88b0974391d
ClaireYarber <clyarber@ou.edu>
2024-12-11 23:06:05 -0600

    Update model-gp-by-f1driver.php

--- model-gp-by-f1driver.php
+++ model-gp-by-f1driver.php
@@ -2,7 +2,7 @@
 function selectgpbyf1driver($fid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT r.rank_id, rank_number, total_points, gp_name, country, day_time FROM `rank` r join gp g on g.rank_id = r.rank_id where t.f1driver_id=?");
+        $stmt = $conn->prepare("SELECT r.rank_id, rank_number, total_points, gp_name, country, day_time FROM `rank` r join gp g on g.rank_id = r.rank_id where g.f1driver_id=?");
         $stmt->bind_param("i", $fid);
         $stmt->execute();
         $result = $stmt->get_result();
