93d1ac597b4a54d30b05fa4e617ebed0ceb0b061
ClaireYarber <clyarber@ou.edu>
2024-12-11 22:20:37 -0600

    Update and rename books-by-author.php to model-f1driver-with-gp.php

---
+++ model-f1driver-with-gp.php
@@ -0,0 +1,32 @@
+<?php
+function selectf1drivers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT f1driver_id, f1driver_name, country FROM `f1driver` ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
+<?php
+function selectgpbyf1driver($fid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT r.rank_id, rank_number, total_points, gp_name, country, day_time FROM `rank` r join gp g on g.rank_id = r.rank_id where g.f1driver_id=?");
+        $stmt->bind_param("i", $fid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
+
