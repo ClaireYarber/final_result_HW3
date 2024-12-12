9eef2322af170d6997a268c233a0f53e8864173b
ClaireYarber <clyarber@ou.edu>
2024-12-11 19:39:16 -0600

    Create pages-by-books.php

---
+++ pages-by-books.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-pages-by-book.php");
+
+$pageTitle = "pages by book";
+include "view-header.php";
+$pages = selectpagesBybook($_POST['cid']);
+include "view-pages-by-book.php";
+include "view-footer.php";
+?>
