6ceac9b96888325b5008fb78050f30b73661e8ca
ClaireYarber <clyarber@ou.edu>
2024-12-12 12:32:40 -0600

    Update view-f1-driver-with-gp.php

--- view-f1-driver-with-gp.php
+++ view-f1-driver-with-gp.php
@@ -4,37 +4,45 @@
   <table class="table">
     <thead>
       <tr>
+        <th>Driver Name</th>
+        <th>Grand Prix Details</th>
       </tr>
     </thead>
     <tbody>
       <?php
-
       while ($f1driver = $f1drivers->fetch_assoc()) { 
       ?>
        <div class="card-group">
-  <div class="card">
-    
-    <div class="card-body">
-      <h5 class="card-title"><?php echo $f1driver['f1driver_name']; ?></h5>
-      <p class="card-text">
-      <ul class="list-group">
-        <?php 
-        $gp = selectgpbyf1driver($f1driver['f1driver_id']);
-        while ($gp = $gps->fetch_assoc()) {
-        ?>
-          <li class="list-group-item"><?php echo $gp['rank_number']; ?> - <?php echo $gp['total_points']; ?> - <?php echo $gp['gp_name']; ?> - <?php echo $gp['country']; ?> - <?php echo $gp['day_time']; ?></li>
-        
-        <?php
-        }
-        ?>
-      </ul>
-      </p>
-      <p class="card-text"><small class="text-body-secondary">Country Origin: <?php echo $f1driver['country']; ?></small></p>
-    </div>
-  </div>
-       
-  <?php
-  } 
-  ?>
-  </div>
-    
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo htmlspecialchars($f1driver['f1driver_name']); ?></h5>
+              <p class="card-text">
+              <ul class="list-group">
+                <?php 
+                // Fetch grand prix details for the current driver
+                $gps = selectgpbyf1driver($f1driver['f1driver_id']);
+                // Loop through the grand prix results
+                while ($gp = $gps->fetch_assoc()) {
+                ?>
+                  <li class="list-group-item">
+                    <?php echo htmlspecialchars($gp['rank_number']); ?> - 
+                    <?php echo htmlspecialchars($gp['total_points']); ?> - 
+                    <?php echo htmlspecialchars($gp['gp_name']); ?> - 
+                    <?php echo htmlspecialchars($gp['country']); ?> - 
+                    <?php echo htmlspecialchars($gp['day_time']); ?>
+                  </li>
+                <?php
+                }
+                ?>
+              </ul>
+              </p>
+              <p class="card-text"><small class="text-body-secondary">Country Origin: <?php echo htmlspecialchars($f1driver['country']); ?></small></p>
+            </div>
+          </div>
+       </div>
+      <?php
+      } 
+      ?>
+    </tbody>
+  </table>
+</div>
