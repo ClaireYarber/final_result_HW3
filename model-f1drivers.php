afeaa3b5f1a60b0c07c5fabd6bc9c8162414c171
ClaireYarber <clyarber@ou.edu>
2024-12-11 21:59:10 -0600

    Update and rename model-f1driverss.php to model-f1drivers.php

--- model-f1drivers.php
+++ model-f1drivers.php
@@ -2,7 +2,7 @@
 function selectf1drivers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT f1drivers_id, f1driver_name, country FROM `f1driver` ");
+        $stmt = $conn->prepare("SELECT f1driver_id, f1driver_name, country FROM `f1driver` ");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
