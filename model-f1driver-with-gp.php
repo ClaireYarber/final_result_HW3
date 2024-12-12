4098984fee35facdaed84bc74a52ee8d8060a8ec
ClaireYarber <clyarber@ou.edu>
2024-12-12 14:23:20 -0600

    Update model-f1driver-with-gp.php

--- model-f1driver-with-gp.php
+++ model-f1driver-with-gp.php
@@ -1,4 +1,17 @@
 <?php
+function selectF1Drivers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT f1driver_id, f1driver_name, country FROM `f1driver`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 function insertF1DriverGP($driver_id, $gp_name, $rank_number, $total_points, $country, $day_time) {
     try {
         $conn = get_db_connection();
