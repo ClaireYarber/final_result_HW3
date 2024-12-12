c26c42905a6143ba4283a733694d618c98bd4232
ClaireYarber <clyarber@ou.edu>
2024-12-12 13:42:49 -0600

    Update model-gp-by-f1driver.php

--- model-gp-by-f1driver.php
+++ model-gp-by-f1driver.php
@@ -1,20 +1,3 @@
-// <?php
-// function selectgpbyf1driver($fid) {
-//     try {
-//         $conn = get_db_connection();
-//         $stmt = $conn->prepare("SELECT r.rank_id, rank_number, total_points, gp_name, country, day_time FROM `rank` r join gp g on g.rank_id = r.rank_id where g.f1driver_id=?");
-//         $stmt->bind_param("i", $fid);
-//         $stmt->execute();
-//         $result = $stmt->get_result();
-//         $conn->close();
-//         return $result;
-//     } catch (Exception $e) {
-//         $conn->close();
-//         throw $e;
-//     }
-// }
-// ?>
-
 <?php
 function selectgpbyf1driver($rid) {
     try {
