650c35f54c010ce95c8aabc31e6abb2a1c4109b1
ClaireYarber <clyarber@ou.edu>
2024-12-12 13:17:16 -0600

    Update gp-by-f1driver.php

--- gp-by-f1driver.php
+++ gp-by-f1driver.php
@@ -4,7 +4,7 @@ require_once("model-gp-by-f1driver.php");
 
 $pageTitle = "F1 Grand Prix";
 include "view-header.php";
-$rank = selectgpbyf1driver ($_GET['id']);
+$rank = selectgpbyf1driver ($_GET['rid']);
 include "view-gp-by-f1driver.php";
 include "view-footer.php";
 ?>
