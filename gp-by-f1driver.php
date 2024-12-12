b8134222dd48b55a916cbb2cb63ea5455862b6ce
ClaireYarber <clyarber@ou.edu>
2024-12-12 13:59:45 -0600

    Update gp-by-f1driver.php

--- gp-by-f1driver.php
+++ gp-by-f1driver.php
@@ -1,29 +1,3 @@
-// <?php
-// require_once("util-db.php");
-// require_once("model-gp-by-f1driver.php");
-
-// $pageTitle = "F1 Grand Prix";
-// include "view-header.php";
-
-// // Make sure the 'rid' parameter is set
-// if (isset($_GET['rid'])) {
-//     $rid = $_GET['rid'];
-//     // Get the rank data by the given rank ID
-//     $rank = selectgpbyf1driver($rid);
-    
-//     // Check if data was returned
-//     if ($rank->num_rows > 0) {
-//         include "view-gp-by-f1driver.php";
-//     } else {
-//         echo "No Grand Prix data found for this rank.";
-//     }
-// } else {
-//     echo "Error: Rank ID (rid) is missing.";
-// }
-
-// include "view-footer.php";
-// ?>
-
 <?php
 require_once("util-db.php");
 require_once("model-gp-by-f1driver.php");
@@ -31,24 +5,26 @@ require_once("model-gp-by-f1driver.php");
 $pageTitle = "F1 Grand Prix";
 include "view-header.php";
 
-// Make sure the 'rank_id' parameter is set via POST
-if (isset($_POST['rank_id'])) {
-    $rank_id = $_POST['rank_id']; // Get the rank_id from the form submission
+// Make sure the 'rid' parameter is set
+if (isset($_GET['rid'])) {
+    $rid = $_GET['rid'];
     // Get the rank data by the given rank ID
-    $rank = selectgpbyf1driver($rank_id);
+    $rank = selectgpbyf1driver($rid);
     
     // Check if data was returned
     if ($rank->num_rows > 0) {
-        include "view-gp-by-f1driver.php"; // Display the data
+        include "view-gp-by-f1driver.php";
     } else {
         echo "No Grand Prix data found for this rank.";
     }
 } else {
-    echo "Error: Rank ID (rank_id) is missing.";
+    echo "Error: Rank ID (rid) is missing.";
 }
 
 include "view-footer.php";
 ?>
+
+
 
 
 
