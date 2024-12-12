ca26d594fa4fef53e8f3ae3d19da5a5dd8df4213
ClaireYarber <clyarber@ou.edu>
2024-12-12 13:02:34 -0600

    Update f1drivers.php

--- f1drivers.php
+++ f1drivers.php
@@ -4,7 +4,34 @@ require_once("model-f1drivers.php");
 
 $pageTitle = "F1 Drivers";
 include "view-header.php";
-$f1drivers = selectf1drivers();
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertF1Driver($_POST['f1driver_name'], $_POST['country'])) {
+        echo '<div class="alert alert-success" role="alert">Driver added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateF1Driver($_POST['f1driver_name'], $_POST['country'], $_POST['f1driver_id'])) {
+        echo '<div class="alert alert-success" role="alert">Driver edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteF1Driver($_POST['f1driver_id'])) {
+        echo '<div class="alert alert-success" role="alert">Driver deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+$f1drivers = selectF1Drivers();
 include "view-f1drivers.php";
 include "view-footer.php";
 ?>
