c921cd9bf49d309a99dd0d82ceb27304c1812587
ClaireYarber <clyarber@ou.edu>
2024-12-11 20:06:01 -0600

    Update model-books-by-author.php

--- model-books-by-author.php
+++ model-books-by-author.php
@@ -2,7 +2,7 @@
 function selectBooksByAuthor($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT c.book_id, book_number, nook_description, year, month, day FROM `book` c join page s on s.book_id = c.book_id where s.Author_id=?");
+        $stmt = $conn->prepare("SELECT c.book_id, book_number, book_description, year, month, day FROM `book` c join page s on s.book_id = c.book_id where s.Author_id=?");
         $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
