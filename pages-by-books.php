ab07efd3c100c3e5b83e518b3677b5be0465bb30
ClaireYarber <clyarber@ou.edu>
2024-12-11 20:09:32 -0600

    Update pages-by-books.php

--- pages-by-books.php
+++ pages-by-books.php
@@ -4,7 +4,7 @@ require_once("model-pages-by-book.php");
 
 $pageTitle = "pages by book";
 include "view-header.php";
-$pages = selectpagesBybook($_POST['cid']);
+$pages = selectpagesBybook($_POST['bid']);
 include "view-pages-by-book.php";
 include "view-footer.php";
 ?>
