c9aaa8dfed567a9958b7514ae098d20160d89421
ClaireYarber <clyarber@ou.edu>
2024-12-11 22:14:34 -0600

    Update and rename authors-with-books.php to f1drivers-with-gp.php

---
+++ f1drivers-with-gp.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-f1drivers-with-gp.php");
+
+$pageTitle = "F1 Drivers Grand Prix";
+include "view-header.php";
+$womenstennispros = selectf1drivers ();
+include "view-f1drivers-with-gp.php";
+include "view-footer.php";
+?>
