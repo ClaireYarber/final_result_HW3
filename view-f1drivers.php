5f61b9506f10d9fc45ccb25a9c9336e22f50af96
ClaireYarber <clyarber@ou.edu>
2024-12-11 21:57:34 -0600

    Update and rename view-authors.php to view-f1drivers.php

---
+++ view-f1drivers.php
@@ -0,0 +1,28 @@
+<<h1>F1 Drivers</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Country</th>
+        <th></th>   
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+
+      while ($f1driver = $f1drivers->fetch_assoc()) { 
+      ?>
+        <tr>
+          <td><?php echo $f1driver['f1driver_id']; ?></td>
+          <td><?php echo $f1driver['f1driver_name']; ?></td>
+          <td><?php echo $f1driver['country']; ?></td>
+          <td><a href="gp-by-f1driver.php?id=<?php echo $f1driver['w_tennispro_id']; ?>">Grand Prix</a></td>
+        </tr>
+      <?php
+      } 
+      ?>
+    </tbody>
+  </table>
+</div>
