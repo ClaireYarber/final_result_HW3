7e1c4ff34eefc4bd18543cc5617a836dfff3cdae
ClaireYarber <clyarber@ou.edu>
2024-12-12 17:02:18 -0600

    Update f1driver-with-gp.php

--- f1driver-with-gp.php
+++ f1driver-with-gp.php
@@ -1,3 +1,42 @@
+// <?php
+// require_once("util-db.php");
+// require_once("model-f1driver-with-gp.php");
+
+// $pageTitle = "F1 Driver Grand Prix";
+// include "view-header.php";
+
+// if (isset($_POST['actionType'])) {
+//   switch ($_POST['actionType']) {
+//     case "Add":
+//       if (insertF1DriverGP($_POST['f1driver_id'], $_POST['gp_name'], $_POST['rank_number'], $_POST['total_points'], $_POST['country'], $_POST['day_time'])) {
+//         echo '<div class="alert alert-success" role="alert">Grand Prix record added.</div>';
+//       } else {
+//         echo '<div class="alert alert-danger" role="alert">Error.</div>';
+//       }
+//       break;
+//     case "Edit":
+//       if (updateF1DriverGP($_POST['f1driver_id'], $_POST['gp_name'], $_POST['rank_number'], $_POST['total_points'], $_POST['country'], $_POST['day_time'], $_POST['rank_id'])) {
+//         echo '<div class="alert alert-success" role="alert">Grand Prix record updated.</div>';
+//       } else {
+//         echo '<div class="alert alert-danger" role="alert">Error.</div>';
+//       }
+//       break;
+//     case "Delete":
+//       if (deleteF1DriverGP($_POST['rank_id'])) {
+//         echo '<div class="alert alert-success" role="alert">Grand Prix record deleted.</div>';
+//       } else {
+//         echo '<div class="alert alert-danger" role="alert">Error.</div>';
+//       }
+//       break;
+//   }
+// }
+
+// $f1drivers = selectf1drivers();
+// $grandPrixRecords = selectGPByF1Driver($f1drivers->fetch_assoc()['f1driver_id']);
+// include "view-f1-driver-with-gp.php";
+// include "view-footer.php";
+// ?>
+
 <?php
 require_once("util-db.php");
 require_once("model-f1driver-with-gp.php");
@@ -8,14 +47,14 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertF1DriverGP($_POST['f1driver_id'], $_POST['gp_name'], $_POST['rank_number'], $_POST['total_points'], $_POST['country'], $_POST['day_time'])) {
+      if (insertF1DriverGP($_POST['f1driver_id'], $_POST['gp_id'], $_POST['gp_name'], $_POST['rank_number'], $_POST['total_points'], $_POST['country'], $_POST['day_time'])) {
         echo '<div class="alert alert-success" role="alert">Grand Prix record added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updateF1DriverGP($_POST['f1driver_id'], $_POST['gp_name'], $_POST['rank_number'], $_POST['total_points'], $_POST['country'], $_POST['day_time'], $_POST['rank_id'])) {
+      if (updateF1DriverGP($_POST['f1driver_id'], $_POST['gp_id'], $_POST['gp_name'], $_POST['rank_number'], $_POST['total_points'], $_POST['country'], $_POST['day_time'], $_POST['rank_id'])) {
         echo '<div class="alert alert-success" role="alert">Grand Prix record updated.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
@@ -31,8 +70,9 @@ if (isset($_POST['actionType'])) {
   }
 }
 
-$f1drivers = selectf1drivers();
+$f1drivers = selectF1Drivers();
 $grandPrixRecords = selectGPByF1Driver($f1drivers->fetch_assoc()['f1driver_id']);
 include "view-f1-driver-with-gp.php";
 include "view-footer.php";
 ?>
+
