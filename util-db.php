f075e31a32b70935fc8ca65196179338dd27257b
ClaireYarber <clyarber@ou.edu>
2024-12-12 12:11:48 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,12 +1,16 @@
 <?php
-function get_db_connection(){
-    // Create connection
-    $conn = new mysqli('138.197.17.168', 'misclair_finalHW3user', 'CY41727jms!', 'misclair_finalHW3');
-    
-    // Check connection
+function get_db_connection() {
+    $host = '138.197.17.168';
+    $username = 'misclair_finalHW3';
+    $password = 'CY41727jms!';
+    $dbname = 'misclair_finalhw3';
+
+    $conn = new mysqli($host, $username, $password, $dbname);
+
     if ($conn->connect_error) {
-      return false;
+        throw new mysqli_sql_exception("Connection failed: " . $conn->connect_error);
     }
+
     return $conn;
 }
 ?>
