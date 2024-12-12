c480a43abf669eea9a55e1e3ebff2bd9da9f5641
ClaireYarber <clyarber@ou.edu>
2024-12-12 12:23:38 -0600

    Update f1driver-with-gp.php

--- f1driver-with-gp.php
+++ f1driver-with-gp.php
@@ -4,7 +4,7 @@ require_once("model-f1driver-with-gp.php");
 
 $pageTitle = "F1 Drivers Grand Prix";
 include "view-header.php";
-$womenstennispros = selectf1drivers ();
+$f1drivers = selectf1drivers ();
 include "view-f1drivers-with-gp.php";
 include "view-footer.php";
 ?>
