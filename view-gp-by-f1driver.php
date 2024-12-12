754c29ffbf4f99bbd43fb92398b463ee0e7c4278
ClaireYarber <clyarber@ou.edu>
2024-12-12 13:35:12 -0600

    Update view-gp-by-f1driver.php

--- view-gp-by-f1driver.php
+++ view-gp-by-f1driver.php
@@ -1,3 +1,30 @@
+// <h1>F1 Grand Prix's</h1>
+// <div class="table-responsive">
+//   <table class="table">
+//     <thead>
+//       <tr>
+//         <th>ID</th>
+//         <th>Number</th>
+//         <th>Points</th>
+//       </tr>
+//     </thead>
+//     <tbody>
+//       <?php
+
+//       while ($ranks = $rank->fetch_assoc()) { 
+//       ?>
+//         <tr>
+//           <td><?php echo $ranks['rank_id']; ?></td>
+//           <td><?php echo $ranks['rank_number']; ?></td>
+//           <td><?php echo $ranks['total_points']; ?></td>
+//         </tr>
+//       <?php
+//       } 
+//       ?>
+//     </tbody>
+//   </table>
+// </div>
+
 <h1>F1 Grand Prix's</h1>
 <div class="table-responsive">
   <table class="table">
@@ -6,21 +33,26 @@
         <th>ID</th>
         <th>Number</th>
         <th>Points</th>
+        <th>Grand Prix</th>
+        <th>Country</th>
+        <th>Date and Time</th>
       </tr>
     </thead>
     <tbody>
       <?php
-
-      while ($ranks = $rank->fetch_assoc()) { 
+      // Loop through the result set and display the data
+      while ($gp = $rank->fetch_assoc()) {
       ?>
         <tr>
-          <td><?php echo $ranks['rank_id']; ?></td>
-          <td><?php echo $ranks['rank_number']; ?></td>
-          <td><?php echo $ranks['total_points']; ?></td>
+          <td><?php echo $gp['rank_id']; ?></td>
+          <td><?php echo $gp['rank_number']; ?></td>
+          <td><?php echo $gp['total_points']; ?></td>
+          <td><?php echo $gp['gp_name']; ?></td>
+          <td><?php echo $gp['country']; ?></td>
+          <td><?php echo $gp['day_time']; ?></td>
         </tr>
-      <?php
-      } 
-      ?>
+      <?php } ?>
     </tbody>
   </table>
 </div>
+
