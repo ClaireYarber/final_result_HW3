ba738a9e702aa5258c12350f961bd8ab75cba991
ClaireYarber <clyarber@ou.edu>
2024-12-11 19:49:23 -0600

    Create model-books-by-author.php

---
+++ model-books-by-author.php
@@ -0,0 +1,16 @@
+<?php
+function selectBooksByAuthor($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT c.book_id, book_number, nook_description, year, month, day FROM `book` c join page s on s.book_id = c.book_id where s.Author_id=?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
