4395e6aae1db9c9d6f912cf9d7b8751da7686ac6
ClaireYarber <clyarber@ou.edu>
2024-12-12 18:52:08 -0600

    Update gp-by-rank.php

--- gp-by-rank.php
+++ gp-by-rank.php
@@ -5,6 +5,6 @@ require_once("model/gp-by-rank.php");
 $pageTitle = "Grand Prix by Rank";
 include "view-header.php";
 $gp = selectgpbyrank ($_POST['rid']);
-include "view-gp-by-rank.php";
-include "view-footer.php";
+include "view/gp-by-rank.php";
+include "view/footer.php";
 ?>
